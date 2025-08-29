bb24fe17431e34db4201ce8d29f313f4a9758d2f
Ambreen <ambreenambi04@gmail.com>
2025-08-29 19:07:56 +0500

    Updated App.js & index.html!

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import FaqItem from './components/FaqItem';
+
+const App = () => {
+
+return (
+      <Routes>
+        <Route path="/" element={<FaqItem />} />
+      </Routes>
+);
+};
+
+export default App;
