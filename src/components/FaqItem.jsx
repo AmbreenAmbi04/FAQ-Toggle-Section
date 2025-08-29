607efb2e706eff20e09a7185b8e547c51297ed0e
Ambreen <ambreenambi04@gmail.com>
2025-08-29 19:19:37 +0500

    Added FAQ questions!

---
+++ src/components/FaqItem.jsx
@@ -0,0 +1,16 @@
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+const FaqItem = ({ question, answer }) => {
+
+const [ isOpen, setIsOpen ] = useState(false);
+
+return (
+    <motion.div>
+        
+    </motion.div>
+)
+
+}
+
+export default FaqItem;
